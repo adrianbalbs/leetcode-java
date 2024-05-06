df09ab8534d30d092492e3edfa0434596a0cbc18
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-05-06 15:58:17 +1000

    Same Tree

---
+++ src/main/java/sametree/Solution.java
@@ -0,0 +1,34 @@
+package sametree;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        }
+
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+        return p.val == q.val && left && right;
+    }
+}
---
+++ src/main/java/sametree/sol.md
@@ -0,0 +1,6 @@
+# Solution
+- Recurse through both trees at the same time and do comparisons
+- Consider base cases where both nodes are null, or if one of the nodes is null
+
+# Time Complexity
+- `O(n + m)`
