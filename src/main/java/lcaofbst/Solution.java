5410d251cf8a43e9b9a4a32ec519858547c51ecf
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-05-07 15:36:32 +1000

    Remove null check

--- src/main/java/lcaofbst/Solution.java
+++ src/main/java/lcaofbst/Solution.java
@@ -21,10 +21,6 @@ class TreeNode {
 
 public class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) {
-            return null;
-        }
-
         if (root.val > p.val && root.val > q.val) {
             return lowestCommonAncestor(root.left, p, q);
         } else if (root.val < p.val && root.val < q.val) {
