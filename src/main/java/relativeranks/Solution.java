ef5177fec50ace79fdc7788bca5f08aef5d0c58b
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-05-08 12:28:00 +1000

    Solve Daily

---
+++ src/main/java/relativeranks/Solution.java
@@ -0,0 +1,31 @@
+package relativeranks;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+record Pair<K, V>(K key, V value) {
+}
+
+public class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        String[] res = new String[score.length];
+
+        PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>((p1, p2) -> p2.key() - p1.key());
+        for (int i = 0; i < score.length; i++) {
+            pq.add(new Pair<>(score[i], i));
+        }
+
+        for (int i = 0; i < score.length; i++) {
+            String place = switch (i) {
+                case 0 -> "Gold Medal";
+                case 1 -> "Silver Medal";
+                case 2 -> "Bronze Medal";
+                default -> String.valueOf(i + 1);
+            };
+            res[pq.remove().value()] = place;
+        }
+        return res;
+    }
+}
---
+++ src/main/java/relativeranks/sol.md
@@ -0,0 +1,6 @@
+# Solution
+- Use Max Heap Priority Queue
+- Make use of a Tuple Record
+
+# Time Complexity
+- `O(nlogn)` as a result of using a Priority Queue.
