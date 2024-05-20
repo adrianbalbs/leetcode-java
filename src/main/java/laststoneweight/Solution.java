a691aab98e70abc84e92cd9af31551b7f6375704
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-05-20 10:07:41 +1000

    Last Stone Weight

---
+++ src/main/java/laststoneweight/Solution.java
@@ -0,0 +1,18 @@
+package laststoneweight;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i : stones) pq.add(i);
+        while (pq.size() > 1) {
+            int y = pq.poll();
+            int x = pq.poll();
+            if (x != y) {
+                pq.add(y - x);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.peek();
+    }}
---
+++ src/main/java/laststoneweight/sol.md
@@ -0,0 +1,5 @@
+# Solution
+- Continuously pop the heaviest two elements of the max heap and follow the cases
+
+# Time Complexity
+- `O(nlogn)`
