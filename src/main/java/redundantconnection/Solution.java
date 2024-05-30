c1279e6a06d48dce4a635d79753171fd1280e704
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-05-30 17:26:52 +1000

    Redundant Connections

---
+++ src/main/java/redundantconnection/Solution.java
@@ -0,0 +1,49 @@
+package redundantconnection;
+
+public class Solution {
+    int[] link;
+    int[] size;
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        link = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; ++i) {
+            link[i] = i;
+            size[i] = 1;
+        }
+
+        for (int[] edge : edges) {
+            if (!union(edge[0] - 1, edge[1] - 1)) {
+                return edge;
+            }
+        }
+        return new int[2];
+    }
+
+    int find(int x) {
+        while (x != this.link[x]) {
+            x = link[x];
+        }
+        return x;
+    }
+    boolean same(int a, int b) {
+        return find(a) == find(b);
+    }
+
+    boolean union(int a, int b) {
+        int s1 = find(a);
+        int s2 = find(b);
+        if (s1 == s2) {
+            return false;
+        }
+
+        if (size[s2] > size[s1]) {
+            link[s1] = s2;
+            size[s2] += size[s1];
+        } else {
+            link[s2] = s1;
+            size[s1] += size[s2];
+        }
+        return true;
+    }
+}
---
+++ src/main/java/redundantconnection/sol.md
@@ -0,0 +1,6 @@
+# Solution
+- Union-find as we are finding cycles in an undirected graph
+- When doing the union decrement the edges by 1 to prevent indexing errors
+
+# Time Complexity
+`O(Elog(V))`
