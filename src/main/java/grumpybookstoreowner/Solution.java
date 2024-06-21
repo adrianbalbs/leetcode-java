c6e99634fa8d8499a48b4a9c07da1e25836b4c0e
Adrian Balbalosa <87012111+adrianbalbs@users.noreply.github.com>
2024-06-21 11:17:07 +1000

    optimise daily

--- src/main/java/grumpybookstoreowner/Solution.java
+++ src/main/java/grumpybookstoreowner/Solution.java
@@ -1,39 +1,29 @@
 package grumpybookstoreowner;
 
-public class Solution {
+class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
         int n = customers.length;
-        int[] maxSatisfactionRange = { 0, 0 };
         int currSatisfaction = 0;
         int maxSatisfactionLost = 0;
-        int l = 0;
 
         for (int r = 0; r < n; r++) {
             if (grumpy[r] == 1) {
                 currSatisfaction += customers[r];
             }
             if (r >= minutes) {
-                if (grumpy[l++] == 1) {
+                if (grumpy[r - minutes] == 1) {
                     currSatisfaction -= customers[r - minutes];
                 }
             }
-            if (currSatisfaction > maxSatisfactionLost) {
-                maxSatisfactionRange[0] = l;
-                maxSatisfactionRange[1] = r;
-                maxSatisfactionLost = currSatisfaction;
-            }
+            maxSatisfactionLost = Math.max(maxSatisfactionLost, currSatisfaction);
         }
 
-        for (int i = maxSatisfactionRange[0]; i <= maxSatisfactionRange[1]; i++) {
-            grumpy[i] = 0;
-        }
-
-        int res = 0;
+        int total = 0;
         for (int i = 0; i < n; i++) {
             if (grumpy[i] == 0) {
-                res += customers[i];
+                total += customers[i];
             }
         }
-        return res;
+        return total + maxSatisfactionLost;
     }
 }
